a7df93731ca6da292cafda7eab5bd2b22c29142b
Orudzhali Nagaev <orudzhali.nagaev@intourist.ru>
2020-10-07 21:51:44 +0300

    Add cli command to create templated vue component, add link to GitHub

--- algorithms/management/commands/vue.py
+++ algorithms/management/commands/vue.py
@@ -3,14 +3,15 @@ import subprocess
 import shutil
 from django.core.management.base import BaseCommand
 from django.conf import settings
-
+from textwrap import dedent
 
 class Command(BaseCommand):
     help = ''
     command = ['vue-cli-service', 'build', '--target', 'lib', '--no-clean', '--filename']
+    _components_dir = os.path.join(settings.BASE_DIR, 'src/components/')
 
     def add_arguments(self, parser):
-        parser.add_argument('action', choices=['build', 'serve'])
+        parser.add_argument('action', choices=['build', 'serve', 'new'])
         parser.add_argument('-n', type=str)
 
     def handle(self, *args, **options):
@@ -33,9 +34,33 @@ class Command(BaseCommand):
                 return
             self.serve_vue_file(pg_name)
 
+        if action == 'new':
+            component_name = options['n'] or 'Empty'
+            src = """
+            <template>
+            </template>
+            
+            <script lang="ts">
+            import Component from 'vue-class-component'
+            import {BaseComponent} from '../utils/base-component'
+            
+            @Component
+            export default class %s extends BaseComponent {
+                created() {
+                }
+            }
+            </script>
+            
+            <style lang="scss" scoped>
+            </style>
+            """ % component_name
+
+            with open(self._components_dir + component_name + '.vue', 'a') as f:
+                f.write(dedent(src))
+
     def build_vue_file(self, pg_name):
         kebab_case_name = self.pascal2kebab(pg_name)
-        subprocess.run([*self.command, kebab_case_name, os.path.join(settings.BASE_DIR, f'src/components/{pg_name}.vue')])
+        subprocess.run([*self.command, kebab_case_name, os.path.join(self._components_dir, f'{pg_name}.vue')])
 
     def serve_vue_file(self, pg_name):
         self.command.insert(2, '--watch')
